c2e5b8e31f65db273c7ade9e95d7e9d9116b0623
andrea-s-hoffman <andrea.s.hoffman09@gmail.com>
2024-05-15 11:10:34 -0400

    deep copy changes

--- shallowVsDeepCopy.js
+++ shallowVsDeepCopy.js
@@ -10,13 +10,29 @@ const ogNumArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const copyNumArr = [...ogNumArr];
 // Using the spread operator (...), a shallow copy of ogNumArr is created. This means that copyNumArr is a new array with the same elements as ogNumArr, but it is a separate instance in memory. Changes to copyNumArr will not affect ogNumArr, and vice versa.
 
-// deep copy
-const deepCopyNumArr = JSON.parse(JSON.stringify(ogNumArr));
-
 copyNumArr.push(123);
 ogNumArr.push(456);
 console.log("ogNumArr:", ogNumArr);
 console.log("copyNumArr:", copyNumArr);
+
+// -------------------------------------------------------------
+const ogObjArr = [
+  { pizza: "cheese", cost: 2.99, vegetarian: true },
+  { pizza: "pepperoni", cost: 3.99, vegetarian: false },
+  { pizza: "margherita", cost: 4.49, vegetarian: true },
+];
+// shallow copy
+// const copyObjArr = [...ogObjArr];
+// with a shallow copy of the array of objects, the object's properties still reference the original objects' memory location in the heap. A change to one property would still affect the original object with a shallow copy.
+
+// deep copy
+const copyObjArr = JSON.parse(JSON.stringify(ogObjArr));
+// However, with a deep copy of the array, the array, objects and their properties are independent from the original data and can be changed without affecting the original array, and vise versa.
+
+ogObjArr[1].cost = 123;
+copyObjArr[0].cost = 456;
+console.log(ogObjArr);
+console.log(copyObjArr);
 
 console.log(
   "-----------------------------------------------------------------------------"
@@ -29,13 +45,28 @@ const ogObj = { a: 1, b: 2, c: 3 };
 // shallow copy
 const copyObj = { ...ogObj };
 
-// deep copy
-const deepCopyObj = JSON.parse(JSON.stringify(ogObj));
-
 copyObj.d = 123;
 ogObj.e = 456;
 console.log("ogObj:", ogObj);
 console.log("copyObj:", copyObj);
+
+// -------------------------------------------------------------
+const ogNestedObj = {
+  question: "What is the world's largest rodent?",
+  correctAnswer: "Capybara",
+  possibleAnswers: ["Capybara", "Rat", "Beaver", "Guinea Pig"],
+};
+
+// shallow copy
+// const copyNestedObj = { ...ogNestedObj };
+
+// deep copy
+const copyNestedObj = JSON.parse(JSON.stringify(ogNestedObj));
+
+ogNestedObj.possibleAnswers[2] = "Toad";
+copyNestedObj.possibleAnswers[3] = "Frog";
+console.log(ogNestedObj);
+console.log(copyNestedObj);
 
 console.log(
   "-----------------------------------------------------------------------------"
