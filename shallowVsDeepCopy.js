0f96d8d8c2cc11a4f66c578c8b1d793399b655a9
andrea-s-hoffman <andrea.s.hoffman09@gmail.com>
2024-05-15 10:40:29 -0400

    oops

--- shallowVsDeepCopy.js
+++ shallowVsDeepCopy.js
@@ -2,13 +2,16 @@
 
 const ogNumArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// shallow copy
+// reference variable
 // const copyNumArr = ogNumArr;
 // If you uncomment this line, copyNumArr would not be a new array but rather a reference to ogNumArr. This means any changes to copyNumArr would also affect ogNumArr, and vice versa, because both variables point to the same memory location in the heap.
 
-// deep copy
+// shallow copy
 const copyNumArr = [...ogNumArr];
 // Using the spread operator (...), a shallow copy of ogNumArr is created. This means that copyNumArr is a new array with the same elements as ogNumArr, but it is a separate instance in memory. Changes to copyNumArr will not affect ogNumArr, and vice versa.
+
+// deep copy
+const deepCopyNumArr = JSON.parse(JSON.stringify(ogNumArr));
 
 copyNumArr.push(123);
 ogNumArr.push(456);
@@ -20,11 +23,14 @@ console.log(
 );
 const ogObj = { a: 1, b: 2, c: 3 };
 
-// shallow copy
+// reference variable
 // const copyObj = ogObj;
 
-// deep copy
+// shallow copy
 const copyObj = { ...ogObj };
+
+// deep copy
+const deepCopyObj = JSON.parse(JSON.stringify(ogObj));
 
 copyObj.d = 123;
 ogObj.e = 456;
